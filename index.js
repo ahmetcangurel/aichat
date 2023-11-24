8b9a8fcbe8551940ff8ed4fb3b2bd776693077ff
Can Gürel <91556629+ahmetcangurel@users.noreply.github.com>
2023-11-25 00:24:58 +0300

    Add => default prop for scrollview and keyboardavoidingview

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, ScrollView, KeyboardAvoidingView} from 'react-native';
 
 TouchableOpacity.defaultProps = {
   activeOpacity: 0.7,
+};
+
+ScrollView.defaultProps = {
+  showsVerticalScrollIndicator: false,
+  showsHorizontalScrollIndicator: false,
+};
+
+KeyboardAvoidingView.defaultProps = {
+  behavior: Platform.OS === 'ios' ? 'padding' : 'height',
+  enabled: true,
+  style: {flex: 1},
 };
 
 AppRegistry.registerComponent(appName, () => App);
