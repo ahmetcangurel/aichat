fc9cf1fa37f7d9c3380a73fed609a628c8adb18d
Can Gürel <91556629+ahmetcangurel@users.noreply.github.com>
2023-11-29 23:25:11 +0300

    Fixed => gesture enabled = false

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -29,9 +29,21 @@ const AuthStack = () => {
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
-      <Stack.Screen name="TellAbout" component={TellAboutScreen} />
-      <Stack.Screen name="VerifyPhone" component={VerifyPhoneScreen} />
-      <Stack.Screen name="EnterVerifyCode" component={EnterVerifyCodeScreen} />
+      <Stack.Screen
+        name="TellAbout"
+        component={TellAboutScreen}
+        options={{gestureEnabled: false}}
+      />
+      <Stack.Screen
+        name="VerifyPhone"
+        component={VerifyPhoneScreen}
+        options={{gestureEnabled: false}}
+      />
+      <Stack.Screen
+        name="EnterVerifyCode"
+        component={EnterVerifyCodeScreen}
+        options={{gestureEnabled: false}}
+      />
     </Stack.Navigator>
   );
 };
